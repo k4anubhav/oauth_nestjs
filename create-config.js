30811d44f85a2dfa49ba7b95b5aab2aaeced3829
Anubhav Kushwaha <k04kushwaha@gmail.com>
2022-05-15 22:37:44 +0530

    redeploy

--- create-config.js
+++ create-config.js
@@ -7,7 +7,6 @@ dotenv.config();
 let config = process.env.ORM_CONFIG;
 console.log(process.env.DATABASE_URL, 'DATABASE_URL');
 if (process.env.DATABASE_URL) {
-  console.log('Using DATABASE_URL');
   config = config.replace('{DATABASE_URL}', process.env.DATABASE_URL);
 }
 
